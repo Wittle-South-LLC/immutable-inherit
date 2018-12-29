01ba44d837f17c9dbe19bc81173c755b157da547
Eric Wittle <eric@wittle.net>
2018-12-29 12:44:57 -0500

    Enhance tests cover all capabilities

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ ImmutableInherit
 
 This package provides a single class, ImmutableInherit, that can be used 
 in Immutable collections, but be extended through inheritance. Specifically,
-it implements Immutable behavior for *is*, *toJS()*, 
+it implements Immutable behavior for *is*, *toJS()*, etc.
 
 It takes a single argument that can be:
 
@@ -11,4 +11,31 @@ It takes a single argument that can be:
 * A JavaScript object
 * A JavaScript list
 
-Will finish this later.
+Whatever you pass as an argument to the constructor will be converted to an
+Immutable object (via fromJS) if it isn't already immutable (tested via 
+isImmutable()). The argument will be available via the getData() method,
+or to subclasses via the _data property.
+
+If called without an argument, the data element will be an empty Map.
+
+Example
+-------
+
+You can extend ImmutableInherit with additional properties, as long you
+ensure that any methods that set value return a new copy of the object.
+So assuming that you might want an immutable object that knows whether it
+is newly created in a client but not persisted. 
+
+class ClientObject extends ImmutableInherit {
+    constructor(createFrom) {
+        super(createFrom)
+        this._new = false
+    }
+    setNew(newVal) {
+        return !this._new === newVal 
+            ? Object.assign(new this.constructor(this._data), {
+                _new: newVal,
+        })
+      : this
+    }
+}
--- tests/test-ImmutableInherit.js
+++ tests/test-ImmutableInherit.js
@@ -1,24 +1,79 @@
 /* test-ImmutableInherit.js - Unit tests for ImmutableInherit */
 
-import ImmutableInherit from '../dist/ImmutableInherit'
-import { Map } from 'immutable'
+import { describe, it, before } from 'mocha'
+import chai from 'chai'
+import ImmutableInherit from '../src/ImmutableInherit'
+import { is, Map, List } from 'immutable'
 
-var errorCount = 0
+const testObj = {key: "value"}
+const testMap = new Map(testObj)
+const testArray = ['one', 'two']
+const testList = new List(testArray)
 
-const testObj = new ImmutableInherit({key: "value"})
+describe('ImmutableInherit created from javascript object', () => {
+  it('.getData() returns Map of supplied object', () => {
+    const testImmutableInherit = new ImmutableInherit(testObj)
+    chai.expect(is(testImmutableInherit.getData(),testMap)).to.be.true
+  })
+  it('.isImmutable() returns true', () => {
+    const testImmutableInherit = new ImmutableInherit(testObj)
+    chai.expect(testImmutableInherit.isImmutable()).to.be.true    
+  })
+  it('.equals() for the same object returns true', () => {
+    const testImmutableInherit = new ImmutableInherit(testObj)
+    chai.expect(testImmutableInherit.equals(testImmutableInherit)).to.be.true    
+  })
+  it('.equals() two separate objects created from the same Immutable object returns true', () => {
+    const t1 = new ImmutableInherit(testMap)
+    const t2 = new ImmutableInherit(testMap)
+    chai.expect(t1.hashCode() !== t2.hashCode()).to.be.true
+    chai.expect(t1._data === t2._data).to.be.true
+    chai.expect(t1.equals(t2)).to.be.true    
+  })
+  it('.equals() two separate objects created from the same Javascript object returns true', () => {
+    const t1 = new ImmutableInherit({key: 'newValue'})
+    const t2 = new ImmutableInherit({key: 'newValue'})
+    chai.expect(t1.hashCode() !== t2.hashCode()).to.be.true
+    chai.expect(t1._data !== t2._data).to.be.true
+    chai.expect(t1.equals(t2)).to.be.true    
+  })
+  it('.equals() two separate objects created from two different javascript object returns false', () => {
+    const t1 = new ImmutableInherit({key: 'Value1'})
+    const t2 = new ImmutableInherit({key: 'Value2'})
+    chai.expect(t1.equals(t2)).to.be.false
+  })
+})
 
-if (!testObj.isImmutable()) {
-    console.log('Could not create ImmutableInherit from JS object')
-    errorCount += 1
-}
-if (!testObj.getData() instanceof Map) {
-    console.log('JS Object: testObj.getData() did not return a Map')
-    errorCount += 1
-}
-if (testObj.getData().get('key') != 'value') {
-    console.log('JS Object: testObj.getData().get(key) did not return value')
-    errorCount += 1
-}
-if (errorCount === 0) {
-    console.log('All tests passed!')
-}
+describe('ImmutableInherit created from Map', () => {
+  it('.getData() returns supplied object', () => {
+    const testImmutableInherit = new ImmutableInherit(testMap)
+    chai.expect(is(testImmutableInherit.getData(),testMap)).to.be.true
+  })
+})
+
+describe('ImmutableInherit created from array', () => {
+  it('.getData() returns List supplied object', () => {
+    const testImmutableInherit = new ImmutableInherit(testArray)
+    chai.expect(is(testImmutableInherit.getData(),testList)).to.be.true
+  })
+})
+
+describe('ImmutableInherit created from List', () => {
+  it('.getData() returns supplied object', () => {
+    const testImmutableInherit = new ImmutableInherit(testArray)
+    chai.expect(is(testImmutableInherit.getData(),testList)).to.be.true
+  })
+})
+
+describe('ImmutableInherit created with no argument', () => {
+  it('.getData() returns empty Map', () => {
+    const testImmutableInherit = new ImmutableInherit()
+    chai.expect(is(testImmutableInherit.getData(),Map({}))).to.be.true
+  })
+})
+
+describe('ImmutableInherit created with simple variable throws exception', () => {
+  it('.getData() returns empty Map', () => {
+    chai.expect(() => {new ImmutableInherit("string")}).to.throw(Error)
+  })
+})
